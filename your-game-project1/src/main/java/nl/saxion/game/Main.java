d1fae55babb41454ce311e4426035e75afa572d3
RutgerAW <rutger.wijnberg@outlook.com>
2025-11-24 13:46:30 +0100

    Add files via upload

---
+++ your-game-project1/src/main/java/nl/saxion/game/Main.java
@@ -0,0 +1,19 @@
+package nl.saxion.game;
+
+import nl.saxion.game.yourgamename.Methodes;
+import nl.saxion.game.yourgamename.YourGameScreen;
+import nl.saxion.game.yourgamename.MainMenuScreen;
+import nl.saxion.gameapp.GameApp;
+import static nl.saxion.game.yourgamename.Methodes.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Add screens
+        GameApp.addScreen("MainMenuScreen", new MainMenuScreen());
+        GameApp.addScreen("YourGameScreen", new YourGameScreen());
+
+        // Start game loop and show main menu screen
+        GameApp.start("Your Game Name", 800, 450, 60, false, "MainMenuScreen");
+        john();
+    }
+}
---
+++ your-game-project1/src/main/java/nl/saxion/game/yourgamename/Methodes.java
@@ -0,0 +1,10 @@
+package nl.saxion.game.yourgamename;
+
+import nl.saxion.gameapp.GameApp;
+
+public class Methodes {
+
+    public static void john(){
+        System.out.println("Hoi");
+    }
+}
