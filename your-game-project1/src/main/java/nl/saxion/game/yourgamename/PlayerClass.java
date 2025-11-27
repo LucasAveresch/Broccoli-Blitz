4c780484dce262fbbc0dfd15beaa92cf8987d3ab
maxko <maxkotteman@gmail.com>
2025-11-27 14:37:12 +0100

    Voor alle PlayerClass aangemaakt

--- your-game-project1/src/main/java/nl/saxion/game/yourgamename/PlayerClass.java
+++ your-game-project1/src/main/java/nl/saxion/game/yourgamename/PlayerClass.java
@@ -1,10 +1,7 @@
 package nl.saxion.game.yourgamename;
 
 public class PlayerClass {
-    int yPosition = 0;
-    int coinsPickedUp= 0;
-    public PlayerClass (){
-        this.yPosition = 200;
-    }
+    int yPosition = 200;
+    int coinsPickedUp;
 }
 
