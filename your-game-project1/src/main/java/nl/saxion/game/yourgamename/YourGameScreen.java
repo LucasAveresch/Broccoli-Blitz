7b5eacabb3ff0c26f0a499d64b7b55c3243ed3f9
RutgerAW <rutger.wijnberg@outlook.com>
2025-11-27 13:15:57 +0100

    Ondergrond + import in game screen

--- your-game-project1/src/main/java/nl/saxion/game/yourgamename/YourGameScreen.java
+++ your-game-project1/src/main/java/nl/saxion/game/yourgamename/YourGameScreen.java
@@ -1,5 +1,6 @@
 package nl.saxion.game.yourgamename;
 
+import com.badlogic.gdx.Screen;
 import nl.saxion.gameapp.GameApp;
 import nl.saxion.gameapp.screens.ScalableGameScreen;
 import static nl.saxion.game.yourgamename.Methodes_Rutger.*;
@@ -11,7 +12,7 @@ public class YourGameScreen extends ScalableGameScreen {
     public YourGameScreen() {
         super(1280, 720); // resolutie 1280x720
     }
-
+    int yPlayer = 200;
     @Override
     public void show() {
         // Geen extra setup nodig
@@ -24,6 +25,7 @@ public class YourGameScreen extends ScalableGameScreen {
         // Scherm volledig zwart maken
         GameApp.clearScreen("black");
         Underground();
+        Player(yPlayer);
     }
 
     @Override
