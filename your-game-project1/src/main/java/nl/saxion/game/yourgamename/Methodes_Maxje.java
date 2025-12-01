1aaafd59339323f11f2cb60240985a0c6460dc49
maxko <maxkotteman@gmail.com>
2025-12-01 16:00:34 +0100

    Voor alle PlayerClass aangemaakt

--- your-game-project1/src/main/java/nl/saxion/game/yourgamename/Methodes_Maxje.java
+++ your-game-project1/src/main/java/nl/saxion/game/yourgamename/Methodes_Maxje.java
@@ -6,7 +6,18 @@ import nl.saxion.game.yourgamename.PlayerClass.*;
 public class Methodes_Maxje {
 
 
-    public static void initializePlayer(int positie){
+    public static void updateEnenmy(float delta, EnemyClass enemyClass) {
+        while (enemyClass.enemyIsDead) {
+            enemyClass.enemyXPos -= enemyClass.enemyspeed * delta;
 
+            GameApp.drawTexture(enemyClass.textureKey, enemyClass.enemyXPos, enemyClass.enemyYPos);
+
+            if (enemyClass.enemyXPos <= 500) {
+                enemyClass.enemyIsDead = true;
+
+            }
+
+        }
+        GameApp.disposeTexture(enemyClass.textureKey);
     }
 }
