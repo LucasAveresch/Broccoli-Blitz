b295af20a464573dd6eebc959e6ab84842332e46
maxkottemann <maxkotteman@gmail.com>
2025-12-04 12:46:04 +0100

    Refactor EnemyClass with new attributes and constructor

--- your-game-project1/src/main/java/nl/saxion/game/yourgamename/EnemyClass.java
+++ your-game-project1/src/main/java/nl/saxion/game/yourgamename/EnemyClass.java
@@ -1,8 +1,26 @@
 package nl.saxion.game.yourgamename;
 
-public class EnemyClass {
-    int enemySpawnx = 0;
-    int enemySpawny = 0;
-    boolean enemyIsdead = false;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import nl.saxion.gameapp.GameApp;
 
+import java.util.ArrayList;
+
+public class EnemyClass {
+    public float enemyXPos;
+    public float enemyYPos;
+    public float enemyspeed;
+    public boolean enemyIsDead = false;
+    public String textureKey;
+
+
+    public EnemyClass(String filepath, String textureKey, float startX, float startY, float speed) {
+        this.textureKey = textureKey;
+        this.enemyXPos = startX;
+        this.enemyYPos = startY;
+        this.enemyspeed = speed;
+
+        GameApp.addTexture(textureKey, filepath);
+    }
 }
+
