e5a85774c10b77258c9662f809370c55938d43e6
maxko <maxkotteman@gmail.com>
2025-12-08 12:33:39 +0100

    Voor alle PlayerClass aangemaakt

---
+++ your-game-project1/src/main/java/nl/saxion/game/yourgamename/PowerupClass.java
@@ -0,0 +1,13 @@
+package nl.saxion.game.yourgamename;
+
+public class PowerupClass {
+    float xPostion;
+    float yposition;
+    String filepath;
+    String textureName;
+
+    public PowerupClass(String filepath, String textureName){
+        this.filepath = filepath;
+
+    }
+}
